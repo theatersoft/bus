baab42afa6edbae910b7e3d429ee87da8e6b7e13
Rob Huehn <rhuehn@gmail.com>
2016-11-07 13:35:25 -0800

    export mixinEventEmitter

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -1,29 +1,36 @@
-export default class EventEmitter {
-    constructor () {
-        this.events /*: Map<Event, Array<Callback>>*/ = new Map()
-    }
+class Base {}
 
-    // aka addListener
-    on (type, callback) {
-        this.events.has(type) || this.events.set(type, [])
-        this.events.get(type).push(callback)
-        return this
-    }
-
-    // aka removeListener
-    off (type, callback) {
-        const callbacks = this.events.get(type)
-        if (callbacks && callbacks.length)
-            this.events.set(type, callbacks.filter(cb => cb !== callback))
-    }
-
-    emit (type, ...args) {
-        const callbacks = this.events.get(type)
-        if (callbacks && callbacks.length) {
-            callbacks.forEach(cb =>
-                cb(...args))
-            return true
+export function mixinEventEmitter (Base) {
+    return class EventEmitter extends Base {
+        constructor (...args) {
+            super(...args)
+            this.events /*: Map<Event, Array<Callback>>*/ = new Map()
         }
-        return false
+
+        // aka addListener
+        on (type, callback) {
+            this.events.has(type) || this.events.set(type, [])
+            this.events.get(type).push(callback)
+            return this
+        }
+
+        // aka removeListener
+        off (type, callback) {
+            const callbacks = this.events.get(type)
+            if (callbacks && callbacks.length)
+                this.events.set(type, callbacks.filter(cb => cb !== callback))
+        }
+
+        emit (type, ...args) {
+            const callbacks = this.events.get(type)
+            if (callbacks && callbacks.length) {
+                callbacks.forEach(cb =>
+                    cb(...args))
+                return true
+            }
+            return false
+        }
     }
 }
+
+export default class EventEmitter extends mixinEventEmitter(Base) {}
--- src/bundle.js
+++ src/bundle.js
@@ -1,4 +1,4 @@
 export {default} from './bus'
 export {proxy} from './proxy'
-export {default as EventEmitter} from './EventEmitter'
+export {default as EventEmitter, mixinEventEmitter} from './EventEmitter'
 export {default as executor} from './executor'
