36e380fdc7ae2ec8a133e82f4c75c513396af9bc
Rob Huehn <rhuehn@gmail.com>
2017-05-04 10:02:08 -0700

    rollup fixed; use arrow function mixin form

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -1,36 +1,34 @@
-class Base {}
+export const mixinEventEmitter = base => class extends base {
+    constructor (...args) {
+        super(...args)
+        this.events /*: Map<Event, Array<Callback>>*/ = new Map()
+    }
 
-export function mixinEventEmitter (Base) {
-    return class EventEmitter extends Base {
-        constructor (...args) {
-            super(...args)
-            this.events /*: Map<Event, Array<Callback>>*/ = new Map()
-        }
+    // aka addListener
+    on (type, callback) {
+        this.events.has(type) || this.events.set(type, [])
+        this.events.get(type).push(callback)
+        return this
+    }
 
-        // aka addListener
-        on (type, callback) {
-            this.events.has(type) || this.events.set(type, [])
-            this.events.get(type).push(callback)
-            return this
-        }
+    // aka removeListener
+    off (type, callback) {
+        const callbacks = this.events.get(type)
+        if (callbacks && callbacks.length)
+            this.events.set(type, callbacks.filter(cb => cb !== callback))
+    }
 
-        // aka removeListener
-        off (type, callback) {
-            const callbacks = this.events.get(type)
-            if (callbacks && callbacks.length)
-                this.events.set(type, callbacks.filter(cb => cb !== callback))
+    emit (type, ...args) {
+        const callbacks = this.events.get(type)
+        if (callbacks && callbacks.length) {
+            callbacks.forEach(cb =>
+                cb(...args))
+            return true
         }
-
-        emit (type, ...args) {
-            const callbacks = this.events.get(type)
-            if (callbacks && callbacks.length) {
-                callbacks.forEach(cb =>
-                    cb(...args))
-                return true
-            }
-            return false
-        }
+        return false
     }
 }
+
+class Base {}
 
 export default class EventEmitter extends mixinEventEmitter(Base) {}
