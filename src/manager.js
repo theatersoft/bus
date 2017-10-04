9702bdb298b1ecc189d4239b77459dcd6ca561d6
Rob Huehn <rhuehn@gmail.com>
2017-10-04 08:50:18 -0700

    defer const rejections

--- src/manager.js
+++ src/manager.js
@@ -9,10 +9,10 @@ type Name = string
 type Path = string
 
 const
-    dupNode = Promise.reject('duplicate node'),
-    missingNode = Promise.reject('missing node'),
-    dupName = Promise.reject('duplicate name'),
-    missingName = Promise.reject('missing name')
+    dupNode = () => Promise.reject('duplicate node'),
+    missingNode = () => Promise.reject('missing node'),
+    dupName = () => Promise.reject('duplicate name'),
+    missingName = () => Promise.reject('missing name')
 
 class Manager {
     names:Map<Name, Path>
@@ -33,7 +33,7 @@ class Manager {
     addNode (path:Path):Promise<void> {
         if (this.proxy) return this.proxy.addNode(path)
         log('manager.addNode', path)
-        if (this.nodes.has(path)) return dupNode
+        if (this.nodes.has(path)) return dupNode()
         this.nodes.set(path, [])
         return Promise.resolve()
     }
@@ -41,7 +41,7 @@ class Manager {
     removeNode (path:Path):Promise<void> {
         if (this.proxy) return this.proxy.removeNode(path)
         log('manager.removeNode', path)
-        if (!this.nodes.has(path)) return missingNode
+        if (!this.nodes.has(path)) return missingNode()
         return Promise.all(
             (this.nodes.get(path) || [])
                 .slice()
@@ -56,17 +56,17 @@ class Manager {
     addName (name:Name, _sender:Path):Promise<void> {
         if (this.proxy) return this.proxy.addName(name)
         log('manager.addName', name)
-        if (this.names.has(name)) return dupName
+        if (this.names.has(name)) return dupName()
         this.names.set(name, _sender)
         const sender = this.nodes.get(_sender)
-        if (!sender) return missingNode
+        if (!sender) return missingNode()
         sender.push(name)
         return Promise.resolve()
     }
 
     resolveName (name:Name):Promise<Name|void> {
         if (this.proxy) return this.proxy.resolveName(name)
-        if (!this.names.has(name)) return missingName
+        if (!this.names.has(name)) return missingName()
         log('manager.resolveName', name, this.names.get(name))
         return Promise.resolve(this.names.get(name))
     }
@@ -74,16 +74,16 @@ class Manager {
     removeName (name:Name, _sender?:Path):Promise<void> {
         if (this.proxy) return this.proxy.removeName(name)
         log('manager.removeName', name)
-        if (!this.names.has(name)) return missingName
+        if (!this.names.has(name)) return missingName()
         const path = this.names.get(name)
-        if (!path) return missingName
+        if (!path) return missingName()
         this.names.delete(name)
         // TODO check path===_sender
-        if (!this.nodes.has(path)) return missingNode
+        if (!this.nodes.has(path)) return missingNode()
         const names = this.nodes.get(path)
-        if (!names) return missingName
+        if (!names) return missingName()
         const i = names.indexOf(name)
-        if (i === -1) return missingName
+        if (i === -1) return missingName()
         names.splice(i, 1)
         return Promise.resolve()
     }
