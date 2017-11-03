4c2c0d7b35f2417b22427eccd9b943df0d9d29e4
Rob Huehn <rhuehn@gmail.com>
2017-11-03 09:45:23 -0700

    tidy

--- src/types.js
+++ src/types.js
@@ -1,41 +1,42 @@
 export type Listener = (...args:any[]) => void
 
-export interface Connection {
-    id:number;
-    name:string;
-    send(data:any):void;
-    close():void;
-    registered:boolean;
-    hello():void;
-    on(type:string, callback:Listener):Connection;
+export
+interface Connection {
+    id: number;
+    name: string;
+    send (data:any): void;
+    close (): void;
+    registered: boolean;
+    hello (): void;
+    on (type:string, callback:Listener): Connection;
 }
 
 export type Request = {
-    path:string,
-    intf:string,
-    member:string,
-    args:mixed[]
+    path: string,
+    intf: string,
+    member: string,
+    args: mixed[]
 }
 
 export type Req = {
-    path:string,
-    intf:string,
-    member:string,
-    args:mixed[],
-    id:number,
-    sender:string
+    path: string,
+    intf: string,
+    member: string,
+    args: mixed[],
+    id: number,
+    sender: string
 }
 
 export type Res = {
-    id:number,
-    path:string,
+    id: number,
+    path: string,
     res?: mixed,
     err?: mixed
 }
 
 export type Sig = {
-    name:string,
-    args:mixed[]
+    name: string,
+    args: mixed[]
 }
 
 export type Data = {|req:Req|} | {|res:Res|} | {|sig:Sig|}
