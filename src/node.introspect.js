18fcd18879ecf5ffb6bff1d697f9a97983c36034
Rob Huehn <rhuehn@gmail.com>
2017-11-03 19:53:29 -0700

    add node name

--- src/node.introspect.js
+++ src/node.introspect.js
@@ -5,6 +5,7 @@ export const nodeIntrospect = (Base:Node) => class extends Base {
     introspect (path:string) {
         if (path !== this.name) return this.request({path, intf: '*', member: 'introspect', args: [path]})
         return {
+            name: this.name,
             children: this.conns
                 .filter((c, i) => i && c && c.name)
                 .map(({name}) => name),
